453b6c16c9a74fca90b718aca5790ab1cb3d80ba
Nodirbek2211 <72076091+Nodirbek2211@users.noreply.github.com>
2021-05-28 19:18:13 +0500

    deleted

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom'
+
+
+const a = 21
+const b = 23
+class Index extends Component {
+
+    sub=()=>{
+        console.log(a-b)
+    }
+    mult=()=>{
+        console.log(a*b)
+    }
+    divition=()=>{
+        console.log(a/b)
+    }
+    render() {
+        return (
+            <div className={'container col-md-4 mt-4'}>
+                <button className={'btn btn-dark mx-3'} onClick={this.sub}>a-b</button>
+                <button className={'btn btn-dark mx-3'} onClick={this.mult}>a*b</button>
+                <button className={'btn btn-dark mx-3'} onClick={this.divition}>a/b</button>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(
+    <Index/>,
+    document.getElementById('root')
+)
